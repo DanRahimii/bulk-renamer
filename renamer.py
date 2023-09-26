cdec193987bc0b547f1ea81869e6dbbc77b34d78
daniel-rahimi <d.rahimi@protonmail.com>
2023-09-26 09:47:04 +0330

    source and target have been added to code.

--- renamer.py
+++ renamer.py
@@ -3,7 +3,7 @@ import os
 def renamer(front,end,source,target):
         #generate a list of the texts needs to be changed
         word_list=[]
-        with open('your_file.txt', 'r') as file:
+        with open(source+'.txt', 'r') as file:
                 for line in file:
                         word = line.strip()  # Remove leading/trailing whitespace, if any
                         word_list.append(word)
@@ -16,9 +16,9 @@ def renamer(front,end,source,target):
         final_list_line_by_line= '\n'.join(final_list)
 
         # import final sentences in final file
-        with open('final.txt', 'w') as file:
+        with open(target+'.txt', 'w') as file:
                 file.write(final_list_line_by_line)
 
         print("Final file has been modified and created successfully!")
 
-renamer()
+renamer(5,4,'file','final')
